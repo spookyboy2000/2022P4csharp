3a4c09dcefba36d52526994feba2fb9fe836fffe
Nick <32973@ma-web.nl>
2022-05-09 14:18:43 +0200

    second commit

---
+++ Csharp2Opdrachten/Csharp2Opdrachten/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp2Opdrachten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
